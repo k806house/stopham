058ffc8f4db2a8ce5e7ebf671a2e0aebcfebb1e4
Анастасия Довженко <AAlDovzhenko@sberbank.ru>
2021-10-30 14:17:13 +0300

    fix imports

--- app/core/app.py
+++ app/core/app.py
@@ -1,7 +1,6 @@
 import os
 from flask import Flask
 
-from core.db import create_engine
 from core.handlers.core import construct_blueprint
 
 
@@ -9,6 +8,5 @@ def create_app():
     app_config = os.getenv('APP_SETTINGS', 'core.config.DevConfig')
     app = Flask(__name__)
     app.config.from_object(app_config)
-    app.db = create_engine(app.config['DB_URL'], app.config['DB_ENV'])
     app.register_blueprint(construct_blueprint(), url_prefix='/v1')
     return app
